cf4f12f684dcb66895243738ec5ee5173a72c735
Ali El-Zein <shaclata@gmail.com>
2025-06-19 14:43:16 -0500

    added configurations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var myApiKey = builder.Configuration["MyAPIKey"];
+Console.WriteLine($"MyAPIKey: {myApiKey}");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
